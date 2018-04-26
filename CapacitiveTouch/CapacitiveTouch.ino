6db56417721749e97880250ed4be83b001e54f2c
Sven Dowideit <SvenDowideit@home.org.au>
2018-04-26 22:41:21 +1000

    extract mqtt and wifi init code to a library
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- CapacitiveTouch/CapacitiveTouch.ino
+++ CapacitiveTouch/CapacitiveTouch.ino
@@ -3,15 +3,9 @@ This is un example howto use Touch Intrrerupts
 The bigger the threshold, the more sensible is the touch
 */
 
-#include <WiFi.h>
-#include <PubSubClient.h>
+#include "mqtt.h"
 
-const char* SSID="ASUS";
-const char* PASS="MEGA SHED";
-const char* MQTT="10.10.11.3";
-const char* TOPIC="status/touch/";
-WiFiClient espWifi;
-PubSubClient client(espWifi);
+Mqtt mqtt = Mqtt("ASUS", "MEGA SHED", "10.10.11.2", 1883);
 
 int threshold = 40;
 bool touch0detected = false;
@@ -32,9 +26,8 @@ void gotTouch2(){
 
 void setup() {
   Serial.begin(115200);
-  setup_wifi();
+  mqtt.setup();
   delay(1000); // give me time to bring up serial monitor
-  client.setServer(MQTT, 1883);
   
   Serial.println("ESP32 Touch Interrupt Test");
   touchAttachInterrupt(T0, gotTouch0, threshold);
@@ -43,43 +36,23 @@ void setup() {
 }
 
 void loop(){
-  mqtt();
+  mqtt.loop();
 
   if(touch0detected){
     touch0detected = false;
     Serial.printf("Touch 0 (pin %d) detected\n", T0);
-    client.publish("status/esp32/touch", "0");
+    mqtt.publish("status/esp32/touch", "0");
   }
   if(touch1detected){
     touch1detected = false;
     Serial.printf("Touch 1 (pin %d) detected\n", T1);
-    client.publish("status/esp32/touch", "2");
+    mqtt.publish("status/esp32/touch", "2");
   }
   if(touch2detected){
     touch2detected = false;
     Serial.printf("Touch 2 (pin %d) detected\n", T2);
-    client.publish("status/esp32/touch", "2");
+    mqtt.publish("status/esp32/touch", "2");
   }
   Serial.printf("w %d, %d, %d, %d, %d, %d, %d, %d, %d, %d\n", touchRead(T0), touchRead(T1), touchRead(T2), touchRead(T3), touchRead(T4), touchRead(T5), touchRead(T6), touchRead(T7), touchRead(T8), touchRead(T9));  // get value using T0
   delay(1000);
 }
-
-void mqtt() {
-  while (!client.connected()) {
-    client.connect("testesp32");
-    Serial.println("+");
-  }
-  client.loop();
-}
-
-void setup_wifi() {
-  delay(10);
-  WiFi.begin(SSID, PASS);
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println(".");
-    delay(500);
-  }
-  Serial.println("Connected:");
-  Serial.println( WiFi.localIP());
-}
-
