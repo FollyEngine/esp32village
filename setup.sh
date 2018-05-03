96d0058bb03da47f12be4de1fa0b112a85abcf65
Sven Dowideit <SvenDowideit@home.org.au>
2018-05-03 22:51:53 +1000

    add fritzing to all computers
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- setup.sh
+++ setup.sh
@@ -9,6 +9,7 @@ sudo sed -i~ 's/main$/main contrib non-free/g' /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get upgrade -yq
 sudo apt-get install -yq sudo flashrom mosquitto mosquitto-clients \
+	fritzing fritzing-parts \
 	vim git firmware-linux-free python-serial \
 	firmware-iwlwifi openssh-server \
 	avrdude binutils-avr extra-xdg-menus gcc-avr libftdi1 \
