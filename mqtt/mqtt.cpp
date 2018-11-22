45666683da1ed60f42f04802be7bb4768a6277f4
Sven Dowideit <SvenDowideit@home.org.au>
2018-11-22 19:54:53 +1000

    add a hostprefix for esp8266
    Signed-off-by: Sven Dowideit <SvenDowideit@home.org.au>

--- mqtt/mqtt.cpp
+++ mqtt/mqtt.cpp
@@ -29,7 +29,12 @@ void Mqtt::setup() {
 
     uint8_t mac[6];
     WiFi.macAddress(mac);
-    snprintf(m_Hostname, 255, "esp32-%x%x%x%x%x%x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+#ifdef ESP8266
+    char *hostPrefix = "esp8266";
+#else
+    char *hostPrefix = "esp32";
+#endif
+    snprintf(m_Hostname, 255, "%s-%x%x%x%x%x%x", hostPrefix, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 
 #ifndef ESP8266
     // TODO: does this exist in some other way?
